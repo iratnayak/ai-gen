894d9c23c2e0ad6ad02096ba1486c8477c65bfdf
iratnayak <iratnayake6@gmail.com>
2025-12-25 21:04:15 +0530

    fixec error

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import DeleteButton from "@/components/DeleteButton";
 
+export const dynamic = "force-dynamic";
+
 interface BlogPageProps {
   params: Promise<{ id: string }>;
 }
